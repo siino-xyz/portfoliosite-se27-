02df36d87344554543428a764b55fbb1f7c5de1f
shinoda <siino.tbh@gmail.com>
2022-08-20 23:50:16 +0900

    タグ一覧ページ表示確認

--- pages/blog/tag/[tagId]/page/[pageNumber].tsx
+++ pages/blog/tag/[tagId]/page/[pageNumber].tsx
@@ -1,4 +1,11 @@
-import { getContents, getPosts, getTags, limit } from "libs/blog";
+import Pagination from "components/pagination/pagination";
+import {
+  getCategories,
+  getContents,
+  getPosts,
+  getTags,
+  limit,
+} from "libs/blog";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import React from "react";
 import { MicroCMS_Category, MicroCMS_Tag, Post } from "types";
@@ -7,40 +14,56 @@ type TagPageProps = {
   posts: Post[];
   pager: [];
   selectedTag: MicroCMS_Tag;
+  tags: MicroCMS_Tag[];
   category: MicroCMS_Category[];
   currentPage: number;
-  tags: MicroCMS_Tag[];
 };
 
-const TagIndexPage: NextPage<TagPageProps> = ({
+const TagPage: NextPage<TagPageProps> = ({
   posts,
   pager,
   selectedTag,
-  category,
   currentPage,
+  category,
   tags,
 }) => {
   return (
     <div>
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h3>{post.title}</h3>
-            <h3>{post.tags?.tag_name}</h3>
+        {tags.map((tag) => (
+          <li key={tag.id}>
+            <p>{tag.tag_name}</p>
           </li>
         ))}
       </ul>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.tags?.id}</p>
+          </li>
+        ))}
+      </ul>
+      {posts.length > 0 && (
+        <ul>
+          <Pagination
+            pager={pager}
+            currentPage={currentPage}
+            selectedTag={selectedTag}
+          />
+        </ul>
+      )}
     </div>
   );
 };
 
-export default TagIndexPage;
+export default TagPage;
 
 export const getAllTagPagePaths = async () => {
   const { contents: tags } = await getTags();
   const paths = await Promise.all(
-    tags.map((tag) => {
-      getPosts({ filters: `tags[equals]${tag.id}`, limit: 1 }).then(
+    tags.map((tag) =>
+      getPosts({ filters: `tag[equals]${tag.id}`, limit: 1 }).then(
         ({ totalCount }) => {
           return [...Array(Math.ceil(totalCount / limit)).keys()].map(
             (num) => ({
@@ -51,16 +74,15 @@ export const getAllTagPagePaths = async () => {
             })
           );
         }
-      );
-    })
+      )
+    )
   );
 
   return paths.flat();
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await getAllTagPagePaths();
-
   return {
     paths,
     fallback: false,
@@ -71,7 +93,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const pageNumber: any = params?.pageNumber || "1";
   const tagId = params?.tagId as string;
 
-  const postFilter = tagId !== undefined ? `tags[equals]${tagId}` : undefined;
+  const postFilter = tagId !== undefined ? `tag[equals]${tagId}` : undefined;
   const { posts, pager, category, tags } = await getContents(
     postFilter,
     Number(pageNumber)

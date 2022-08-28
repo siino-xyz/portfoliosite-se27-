93a60ae729c17c87c70ee5b8c7f643968ff9008d
shinoda <b114807h@gmail.com>
2022-08-28 23:29:36 +0900

    dev -> merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage<HomePageProps> = ({ posts }) => {
           />
           <ContentBlock
             sctiontitle="受託開発"
-            body='アイテムは width および height プロパティによって寸法が決められます。寸法は完全に固定で、フレックスコンテナーに連動して収縮したり伸長したりすることはありません。これは"flex: 0 0 auto" と同等です。'
+            body='アイテムは width および height プロパティによって寸法が決められます。寸法は完全に固定で、フレックスコンテナーに連動して収縮したり伸長したりすることはありまん。これは"flex: 0 0 auto" と同等です。'
             src="/IBMPS1.jpg"
             alt="ibm_pc"
           />
